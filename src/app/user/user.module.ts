ec44f89e473c341c8d4fd722a47eec3a62162f00
x-Beo-x <86937244+x-Beo-x@users.noreply.github.com>
2021-11-30 15:40:59 +0700

    add auth module,user cart

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { HomeComponent } from './home/home.component';
-import { MerchantsComponent } from './merchants/merchants.component';
+import { MerchantsComponent } from './merchant-list/merchants.component';
 import { MerchantsDetailComponent } from './merchants-detail/merchants-detail.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, MerchantsComponent, MerchantsDetailComponent, OrderHistoryComponent, CheckoutComponent],
+  declarations: [HomeComponent, MerchantsComponent, MerchantsDetailComponent, OrderHistoryComponent, CheckoutComponent, CartComponent],
   imports: [
     CommonModule,
     UserRoutingModule
