c28b44af62def14e264ce2992de70702864a6e42
vanhopld98 <vanhopld98@gmail.com>
2021-12-01 09:23:40 +0700

    fix navbar home page

---
+++ src/app/shared/navbar-home/navbar-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar-home.component.html',
+  styleUrls: ['./navbar-home.component.css']
+})
+export class NavbarHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
