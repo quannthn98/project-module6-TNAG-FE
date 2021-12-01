d5cfd3bdf3ea8b043ce719a383c75d86abd37225
vanhopld98 <vanhopld98@gmail.com>
2021-12-01 07:47:42 +0700

    done form login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../service/authentication.service';
+import {Router} from '@angular/router';
+import {error} from 'protractor';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authentication: AuthenticationService,
+              private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  login(formLogin) {
+    this.authentication.login(formLogin.value.username, formLogin.value.password).subscribe(() => {
+      this.router.navigate(['/']);
+    }, () => {
+      document.getElementById('errorsLogin').innerHTML = 'Sai tên tài khoản hoặc mật khẩu !';
+    })
+    ;
+  }
 }
