a81c4a7909f6b3cb574f2bfe96c87e541e9558ca
vanhopld98 <vanhopld98@gmail.com>
2021-12-01 14:46:29 +0700

    create category interface & service

---
+++ src/app/service/category.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Category} from '../model/category';
+import {environment} from '../../environments/environment';
+
+const API_URL = environment.API_URL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<Category[]> {
+    return this.http.get<Category[]>(API_URL + '/categories');
+  }
+}
