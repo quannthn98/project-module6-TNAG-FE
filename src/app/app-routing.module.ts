b895f2c924cab2d2f196e6bf607400f8e142b9c7
vanhopld98 <vanhopld98@gmail.com>
2021-12-02 09:00:57 +0700

    chia layout admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {LayoutMerchantComponent} from './layout/layout-merchant/layout-merchant.component';
+import {LayoutAdminComponent} from './layout/layout-admin/layout-admin.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,11 @@ const routes: Routes = [
     path: 'merchant',
     component: LayoutMerchantComponent,
     loadChildren: () => import('./merchant/merchant.module').then(module => module.MerchantModule)
+  },
+  {
+    path: 'admin',
+    component: LayoutAdminComponent,
+    loadChildren: () => import('./admin/admin.module').then(module => module.AdminModule)
   }
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,16 +7,18 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarMerchantComponent } from './navbar-merchant/navbar-merchant.component';
 import { FooterMerchantComponent } from './footer-merchant/footer-merchant.component';
+import { SidebarAdminComponent } from './sidebar-admin/sidebar-admin.component';
 
 
 @NgModule({
-  declarations: [FooterComponent, NavbarComponent, SidebarComponent, NavbarMerchantComponent, FooterMerchantComponent],
+  declarations: [FooterComponent, NavbarComponent, SidebarComponent, NavbarMerchantComponent, FooterMerchantComponent, SidebarAdminComponent],
   exports: [
     FooterComponent,
     NavbarComponent,
     SidebarComponent,
     NavbarMerchantComponent,
-    FooterMerchantComponent
+    FooterMerchantComponent,
+    SidebarAdminComponent
   ],
   imports: [
     CommonModule,
