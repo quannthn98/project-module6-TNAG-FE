020caa979f4467f55063927cfd3b606054a17cbe
vanhopld98 <vanhopld98@gmail.com>
2021-12-02 09:10:50 +0700

    remove user/merchant-detail conflict

--- src/app/user/merchants-detail/merchants-detail.component.ts
+++ src/app/user/merchants-detail/merchants-detail.component.ts
@@ -10,36 +10,10 @@ import {FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./merchants-detail.component.css']
 })
 export class MerchantsDetailComponent implements OnInit {
-  merchant: Merchant = {};
-  id: number;
-  detailMerchantForm: FormGroup = new FormGroup({
-    id: new FormControl(),
-    name: new FormControl(),
-    address: new FormControl(),
-    hotline: new FormControl(),
-    openHours: new FormControl(),
-  });
 
-  constructor(private userService: UserService,
-              private activatedRoute: ActivatedRoute) {
-    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-      this.id = +paramMap.get('id');
-      this.getMerchant(this.id);
-    });
+  constructor() {
   }
 
   ngOnInit() {
-  }
-
-  getMerchant(id: number) {
-    this.userService.getMerchantById(id).subscribe(merchant => {
-      this.detailMerchantForm = new FormGroup({
-        id: new FormControl(merchant.id),
-        name: new FormControl(merchant.name),
-        address: new FormControl(merchant.address),
-        hotline: new FormControl(merchant.hotline),
-        openHours: new FormControl(merchant.openHours)
-      });
-    });
   }
 }
