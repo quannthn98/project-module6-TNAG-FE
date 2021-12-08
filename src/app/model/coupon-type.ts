a46d951b4b62b69e1c8e8f6147cceda54ba856fc
vanhopld98 <vanhopld98@gmail.com>
2021-12-08 14:06:24 +0700

    Thêm model coupon , coupon ttype

--- src/app/model/coupon-type.ts
+++ src/app/model/coupon-type.ts
@@ -1,2 +1,4 @@
 export interface CouponType {
+  id?:number;
+  name?:string;
 }
--- src/app/model/coupon.ts
+++ src/app/model/coupon.ts
@@ -1,2 +1,8 @@
 export interface Coupon {
+  id?: number;
+  discount?: number;
+  inputCode?: string;
+  description?: string;
+  couponType?: any;
+  merchantProfile?: any;
 }
