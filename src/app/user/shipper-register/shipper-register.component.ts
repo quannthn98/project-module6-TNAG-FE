36cbb74d9272dd8c772e2408dab3dbc274ca1997
nguyen dac son <dacson96@gmail.com>
2021-12-10 10:46:27 +0700

    shipper dang ky

--- src/app/user/shipper-register/shipper-register.component.ts
+++ src/app/user/shipper-register/shipper-register.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ShipperProfile} from '../../model/shipper-profile';
+import {UserToken} from '../../model/userToken';
+import {AuthenticationService} from '../../service/authentication.service';
+import {UserService} from '../../service/user.service';
+import {Router} from '@angular/router';
+import {AlertService} from '../../service/alert.service';
 
 @Component({
   selector: 'app-shipper-register',
@@ -8,10 +13,44 @@ import {ShipperProfile} from '../../model/shipper-profile';
 })
 export class ShipperRegisterComponent implements OnInit {
   shipperProfile: ShipperProfile = {};
+  driverLicense: any;
+  idFront: any;
+  idBack: any;
+  ownerCertificate: any;
+  profession: string;
 
-  constructor() { }
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  handleDriverLicenseInput(event) {
+    this.driverLicense = (event.target).files[0];
+  }
+
+  handleIdFrontInput(event) {
+    this.idFront = (event.target).files[0];
+  }
+
+  handleIdBackInput(event) {
+    this.idBack = (event.target).files[0];
+  }
+
+  handleCertificateInput(event) {
+    this.ownerCertificate = (event.target).files[0];
+  }
+
+  submit() {
+    const formData = new FormData();
+    formData.append('driverLicense', this.driverLicense);
+    formData.append('idCardFront', this.idFront);
+    formData.append('idCardBack', this.idBack);
+    formData.append('vehicleOwnershipCertificate', this.ownerCertificate);
+    formData.append('profession', this.profession);
+    this.userService.shipperRegister(formData).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
+
